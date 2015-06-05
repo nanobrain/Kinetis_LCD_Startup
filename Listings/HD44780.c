6e1636825555221d50a2e865d55af49fb9ebfeba
hipis_barefoot <adrian.kurylak@poczta.pl>
2015-06-05 14:18:21 +0200

    code clearing

--- Listings/HD44780.c
+++ Listings/HD44780.c
@@ -1,43 +1,39 @@
-/** ###################################################################
+/** ###############HD44780.c###########################################
 **     Filename  : HD44780.c
 **     Processor : MKL25Z
 **     Date/Time : 2015-31-05, 20:34
-** ###################################################################*/
-//----1----2----3----4----5----6----7-10----11--12--13--14--15----16--LCD
-//-------------------E05--E20--E21-NC--NC---E22-E23-E29-E30-----------Kinetis
-//---Vss--Vcc--Vee---Rs---Rw---E---DB0-DB3--DB4-DB5-DB6-DB7-LED+--LED-
+** ###################################################################
+----1----2----3----4----5----6----7-10----11--12--13--14--15----16--LCD
+-------------------E05--E20--E21-NC--NC---E22-E23-E29-E30-----------Kinetis
+---Vss--Vcc--Vee---Rs---Rw---E---DB0-DB3--DB4-DB5-DB6-DB7-LED+--LED-
+*/
 
 #include "fsl_device_registers.h"
 #include "Declarations.h"
 #include "HD44780.h"
 
-void LCD_IO_PutVal(uint8_t val)
-{
+void LCD_IO_PutVal(uint8_t val){
+	
 	(void)((val & MASK(0)) ? (PTE->PSOR |= MASK(D4)): (PTE->PCOR |= MASK(D4)));
 	(void)((val & MASK(1)) ? (PTE->PSOR |= MASK(D5)): (PTE->PCOR |= MASK(D5)));
 	(void)((val & MASK(2)) ? (PTE->PSOR |= MASK(D6)): (PTE->PCOR |= MASK(D6)));
 	(void)((val & MASK(3)) ? (PTE->PSOR |= MASK(D7)): (PTE->PCOR |= MASK(D7)));
 }
 
-uint8_t LCD_IO_GetVal()
-{
+uint8_t LCD_IO_GetVal(){
+
 	uint8_t temp;
 	
 	(void)((PTE->PDIR & MASK(D7)) ? (temp |=MASK(3)): (temp &=~MASK(3)));
 	(void)((PTE->PDIR & MASK(D6)) ? (temp |=MASK(2)): (temp &=~MASK(2)));
 	(void)((PTE->PDIR & MASK(D5)) ? (temp |=MASK(1)): (temp &=~MASK(1)));
 	(void)((PTE->PDIR & MASK(D4)) ? (temp |=MASK(0)): (temp &=~MASK(0)));
-	/*
-	temp =  (PTE->PDIR & MASK(D7))<<3;
-	temp |= (PTE->PDIR & MASK(D6))<<2;
-	temp |= (PTE->PDIR & MASK(D5))<<1;
-	temp |= (PTE->PDIR & MASK(D4));
-*/
+	
 	return temp;
 }
 
 uint8_t LCD_ReadNibble(void) {
-
+	
 	uint8_t temp=0;
 	
 	Set_Input;
@@ -49,7 +45,7 @@ uint8_t LCD_ReadNibble(void) {
 }
   
 uint8_t LCD_CheckStatus(void){
-
+	
 	uint8_t status = 0;
 	RS_PutVal(0);
 	RW_PutVal(1);
@@ -60,7 +56,7 @@ uint8_t LCD_CheckStatus(void){
 }
 
 void LCD_WriteNibble(uint8_t Data){
-
+	
 	E_PutVal(1);
 	LCD_IO_PutVal(Data);
 	E_PutVal(0);
@@ -78,7 +74,7 @@ void LCD_WriteData(uint8_t DataToWrite){
 	while(LCD_CheckStatus() & 0x80);
 }
 
-void LCD_WriteText(uint8_t * text) {
+void LCD_WriteText(uint8_t * text) {	
 
 	while(*text) {
 	  LCD_WriteData(*text);
@@ -88,7 +84,7 @@ void LCD_WriteText(uint8_t * text) {
 }
 
 void LCD_WriteCommand(uint8_t command){
-
+	
 	E_PutVal(0);
 	RS_PutVal(0);
 	RW_PutVal(0); 
@@ -99,26 +95,26 @@ void LCD_WriteCommand(uint8_t command){
 
 void LCD_Init(void){
 
-	_delay_ms(1000);                            // 50ms dla ustabilizowania siê napiêcia zasilania
-																							// start initialization
-	                                                 
+	_delay_ms(50);														// 50ms dla ustabilizowania siê napiêcia zasilania
+	
+																						// start initialization	                                                 
 	LCD_WriteCommand((uint8_t)LCD_INIT);
-	_delay_ms(1);                              // 1ms, minimum 39us
+	_delay_ms(1);                             // 1ms, minimum 39us
 	
 	LCD_WriteCommand((uint8_t)LCD_FSET);
-	_delay_ms(1);                              // 1ms, minimum 39us
+	_delay_ms(1);                             // 1ms, minimum 39us
 	
 	LCD_WriteCommand((uint8_t)LCD_FSET);
-	_delay_ms(1);                              // 1ms, minimum 37us
+	_delay_ms(1);                             // 1ms, minimum 37us
 	
 	LCD_WriteCommand((uint8_t)LCD_OFFON);
-	_delay_ms(1);                              // 1ms, minimum 37us
+	_delay_ms(1);                             // 1ms, minimum 37us
 	
 	LCD_WriteCommand((uint8_t)LCD_CLR);
-	_delay_ms(2);                              // 2ms, minimum 1.54ms
+	_delay_ms(2);                             // 2ms, minimum 1.54ms
 	
 	LCD_WriteCommand((uint8_t)LCD_EMODE);
-	_delay_ms(1);                              // 1ms (O tym dokumentacja nie wspomina³a....)
+	_delay_ms(1);                             // 1ms (O tym dokumentacja nie wspomina³a....)
 	
-	                                                // stop initialization
+	                                          // stop initialization
 }
--- Listings/main.c
+++ Listings/main.c
@@ -1,33 +1,8 @@
-/*
- * Copyright (c) 2013, Freescale Semiconductor, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of Freescale Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
+/* ################main.c#########################################
+**     Filename  : main.c
+**     Processor : Universal
+**     Date/Time : 2015-31-05, 20:34
+** ###############################################################*/
 #include "fsl_device_registers.h"
 #include "Declarations.h"
 #include "HD44780.h"
@@ -46,8 +21,7 @@ int main(void)
     }    
 }
 
-static void init_hardware(void)
-{    
+static void init_hardware(void){    
 	SIM->SCGC5 |= ( SIM_SCGC5_PORTA_MASK
 								| SIM_SCGC5_PORTB_MASK
 								| SIM_SCGC5_PORTC_MASK
@@ -100,15 +74,13 @@ static void init_hardware(void)
 		
 }
 
-void RGB(uint16_t Red,uint16_t Green,uint16_t Blue)
-{
+void RGB(uint16_t Red,uint16_t Green,uint16_t Blue){
   (void)(Red   ? (FPTB->PCOR |= MASK(LED_R)) : (FPTB->PSOR |= MASK(LED_R)));
 	(void)(Green ? (FPTB->PCOR |= MASK(LED_G)) : (FPTB->PSOR |= MASK(LED_G)));
 	(void)(Blue  ? (FPTD->PCOR |= MASK(LED_B)) : (FPTD->PSOR |= MASK(LED_B)));
 }
 
-void _delay_ms(unsigned delayTicks)
-{    
+void _delay_ms(unsigned delayTicks){    
 	delayTicks *=(uint64_t)SystemCoreClock/10000u;
     while(delayTicks--)
     {
